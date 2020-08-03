63ee78706e7e67e15e253b4961c44217c7168a45
hibana-hq <69167325+hibana-hq@users.noreply.github.com>
2020-08-03 21:16:54 +0100

    summary update all content with new changes to config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter SaaS Marketing`,
-    description: `A simple one page marketing starter for saas companies.`,
-    author: `Keegan Burkett`,
+    title: `Hibana`,
+    description: `Platform for discovering and browsing independent brands.`,
+    author: `Tech Guy`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
