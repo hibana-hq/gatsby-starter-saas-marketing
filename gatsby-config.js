231b6a36b62c3799ff8031a95cffb738937e264b
hibana-hq <69167325+hibana-hq@users.noreply.github.com>
2020-08-03 21:55:31 +0100

    deleted_footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/hibana_logo_h.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
